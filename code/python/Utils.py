93bbb2fb36bd16c427fe0dc530b5ac02fd041c79
Mikail <mikail.yayla@tu-dortmund.de>
2023-04-03 13:26:30 +0200

    Separate data loaders for train and test in imagenette

--- code/python/Utils.py
+++ code/python/Utils.py
@@ -86,7 +86,7 @@ def get_model_and_datasets(args):
         dataset2 = datasets.CIFAR100('data', train=False, transform=transform_test)
 
     if args.dataset == "IMAGENETTE":
-        transform = transforms.Compose([
+        transform_train = transforms.Compose([
             transforms.Resize((64, 64)),
             transforms.RandomCrop(64, padding=4),
             transforms.RandomHorizontalFlip(),
@@ -94,9 +94,16 @@ def get_model_and_datasets(args):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
-        dataset1 = datasets.ImageFolder('data/imagenette2/train', transform=transform)
-        dataset2 = datasets.ImageFolder('data/imagenette2/val', transform=transform)
-    return nn_model, dataset1, dataset2
+        transform_test = transforms.Compose([
+            transforms.Resize((64, 64)),
+            # transforms.RandomCrop(64, padding=4),
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        dataset1 = datasets.ImageFolder('data/imagenette2/train', transform=transform_train)
+        dataset2 = datasets.ImageFolder('data/imagenette2/val', transform=transform_test)
 
 def set_layer_mode(model, mode):
     for layer in model.children():
